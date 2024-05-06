38e81b900174e8155ba59d321e37c6f64e68b956
MSvidlo <143841475+MSvidlo@users.noreply.github.com>
2024-05-06 23:09:32 +0200

    addinf changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
 
 import React from 'react';
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import  ContactFrom from './components/ContactForm/ContactForm';
 import ContacList from './components/ContactList/ContactList';
 import SearchBox from './components/SearchBox/SearchBox';
+import { fetchContacts } from './redux/contactsOps';
 
 const App = () => {
   const dispatch = useDispatch();
-
+  const isLoading = useSelector((state) => state.contacts.isLoading);
+  const error= useSelector((state)=>state.contacts.error)
   useEffect(() => {
-    dispatch()
+    dispatch(fetchContacts())
   }, [dispatch]);
 
   return (
     <div>
       <h1>Phonebook</h1>
       <ContactFrom />
+        {isLoading && !error && <b>Request in progress...</b>}
       <SearchBox />
       <ContacList />
     </div>
--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,5 +1,17 @@
-import { createSlice, nanoid, createSelector } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 import { CONSTANTS } from './constants.js';
+import { fetchContacts,addContact,deleteContact } from "./contactsOps.js";
+import { createSelector } from "@reduxjs/toolkit";
+
+const selectContacts = state => state.contacts.items;
+const selectFilters = state => state.filters.name;
+
+
+export const selectFilteredContacts =createSelector([selectContacts,selectFilters],(contacts,filter)=>{
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  } )
 
 const handlePending = (state) => {
   state.isLoading = true;
@@ -18,15 +30,32 @@ const ontactsSlice = createSlice({
   error:null,},
   extraReducers: (builder) => {
       builder
-        addContact: (state, action) => {
-                state.items.    push(action.payload);
-            },
+         .addCase(fetchContacts.pending, handlePending)
+      .addCase(fetchContacts.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = null;
+        state.items = action.payload;
+      })
+      .addCase(fetchContacts.rejected, handleRejected)
+      .addCase(addContact.pending, handlePending)
+      .addCase(addContact.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = null;
+        state.items.push(action.payload);
+      })
+      .addCase(addContact.rejected, handleRejected)
+      .addCase(deleteContact.pending, handlePending)
+      .addCase(deleteContact.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = null;
+        const index = state.items.findIndex(
+          (contact) => contact.id === action.payload.id
+        );
+        state.items.splice(index, 1);
+      })
+      .addCase(deleteContact.rejected, handleRejected)
+      
+  },
+});
 
-            deleteContact(state, action) {
-state.items.splice(state.items.indexOf(action.payload), 1);
-            },
-        },
-    });
-
-export const { addContact, deleteContact } = ontactsSlice.actions;
 export const contactReducer = ontactsSlice.reducer;
