81f2e32646a0d3310c7d07eda37d9e0d43bb5039
MSvidlo <143841475+MSvidlo@users.noreply.github.com>
2024-05-06 22:16:15 +0200

    started to refactore code
    delete persist datas

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 
 import React from 'react';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import  ContactFrom from './components/ContactForm/ContactForm';
 import ContacList from './components/ContactList/ContactList';
 import SearchBox from './components/SearchBox/SearchBox';
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch()
+  }, [dispatch]);
+
   return (
     <div>
       <h1>Phonebook</h1>
--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,11 +1,23 @@
 import { createSlice, nanoid, createSelector } from "@reduxjs/toolkit";
 import { CONSTANTS } from './constants.js';
-import { persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+
+const handlePending = (state) => {
+  state.isLoading = true;
+};
+
+const handleRejected = (state) => {
+  state.isLoadinf = false;
+  state.error = action.payload;
+}
+
 const ontactsSlice = createSlice({
     name: 'contacts',
-   initialState: { items: CONSTANTS.CONTACTS_INIT },
-    reducers: {
+  initialState: {
+    items: CONSTANTS.CONTACTS_INIT,
+    isLoading: false,
+  error:null,},
+  extraReducers: (builder) => {
+      builder
         addContact: (state, action) => {
                 state.items.    push(action.payload);
             },
@@ -15,15 +27,6 @@ state.items.splice(state.items.indexOf(action.payload), 1);
             },
         },
     });
-const persistContactsConfig = {
-  key: 'root',
-  whitelist: ['items'],
-  storage,
-};
 
 export const { addContact, deleteContact } = ontactsSlice.actions;
 export const contactReducer = ontactsSlice.reducer;
-export const persistedContactsReducer = persistReducer(
-  persistContactsConfig,
-  contactReducer
-);
